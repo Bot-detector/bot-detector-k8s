6002d63b444b37ab78c0d8dea686ffa275e69300
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-03-12 13:33:24 +0100

    init secrets scripts

---
+++ format_deployment_env.py
@@ -0,0 +1,25 @@
+import yaml
+from yaml.loader import SafeLoader
+
+file = './secrets/bd-automation-prd.yaml'
+# Open the file and load the file
+with open(file) as f:
+    data:dict = yaml.load(f, Loader=SafeLoader)
+
+metadata:dict = data.get('metadata')
+secret_name = metadata.get('name')
+secrets:dict = data.get('data')
+
+env = []
+for key in secrets.keys():
+    base = {
+        'name': key,
+        'valueFrom':{
+            'secretKeyRef': {
+                'name': secret_name,
+                'key': key
+            }
+        }
+    }
+    env.append(base)
+print(yaml.dump({'env':env}))
---
+++ format_secret.py
@@ -0,0 +1,12 @@
+import base64
+
+file = './secrets/.env'
+
+with open(file) as fp:
+    line:str = fp.readline()
+    while line:
+        key, value = line.split(sep=" = ")
+        encoded_value = base64.b64encode(value.encode('ascii'))
+
+        print(f'{key}: >- \n \t {str(encoded_value.decode())}')
+        line:str = fp.readline()
